b552cd1f68fd441553bcbc09b8a2a78e35608ba3
SrMazas <info.otero.gabriel@gmail.com>
2022-10-05 16:00:45 +0200

    Save

--- old/race/desktop/src/com/gabriel/game/DesktopLauncher.java
+++ old/race/desktop/src/com/gabriel/game/DesktopLauncher.java
@@ -7,7 +7,8 @@ import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
-		config.setForegroundFPS(60);
+		config.setWindowedMode(800,500);
+//		config.setForegroundFPS(60);
 		config.setTitle("Race");
 		new Lwjgl3Application(new GameManager(), config);
 	}
